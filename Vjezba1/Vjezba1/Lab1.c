a7f5db2f37d2d1a8b7882bd634720bc10235f1c0
goretante <gospodingoreta@gmail.com>
2023-10-11 19:58:47 +0200

    edited Vjezba1

--- Vjezba1/Vjezba1/Lab1.c
+++ Vjezba1/Vjezba1/Lab1.c
@@ -1,7 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE (50)
+#define MAX_LINE (1024)
+#define FILE_ERROR_OPEN (-1)
+
+typedef struct _student {
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+} Student;
+
 
 int main() {
-	printf("Hello World!\n");
 
+	int noRows = 0;
+	FILE* filePointer = NULL;
+	char buffer[MAX_LINE] = { 0 };
+
+	filePointer = fopen("students.txt", "r");
+	if (!filePointer) {
+		printf("Datoteka nije otvorena!");
+		return FILE_ERROR_OPEN;
+	}
+
+	printf("%d", noRows);
+
+	fclose(filePointer);
 	return 0;
 }
