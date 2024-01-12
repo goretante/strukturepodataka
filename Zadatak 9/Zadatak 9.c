af919e859761650777d3c206372dbd2c15865cc3
goretante <gospodingoreta@gmail.com>
2024-01-12 15:35:04 +0100

    task 9 update

--- Zadatak 9/Zadatak 9.c
+++ Zadatak 9/Zadatak 9.c
@@ -1,136 +1,118 @@
-/*
-Zadan je niz brojeva 2, 5, 7, 8, 11, 1, 4, 2, 3, 7 koji su spremljeni u cvorove binarnog stabla.
-	a) Napisati funkciju insert koja dodaje element u stablo tako da se pozivima te funkcije za
-	sve element zadanog niza brojeva stvori stablo kao na slici Slika 1. Funkcije vraca
-	pokazivac na korijen stabla.
-	b) Napisati funkciju replace koja ce svaki element stabla zamijeniti sumom elemenata u
-	njegovom lijevom i desnom podstablu(tj.sumom svih potomaka prije zamjene
-	vrijednosti u tim potomcima).
-	c) Prepraviti program na nacin da umjesto predefiniranog cjelobrojnog polja koristenjem
-	funkcije rand() generira slucajne brojeve u rasponu <10, 90>.Takoder, potrebno je
-	upisati u datoteku sve brojeve u inorder prolasku nakon koristenja funkcije iz a), zatim b)
-	dijela zadatka.
-*/
-
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-// structures
 struct _node;
 typedef struct _node* Position;
+
 typedef struct _node {
-	int element;
-	Position left;
-	Position right;
+    int element;
+    Position left;
+    Position right;
 } Node;
 
-// prototype functions
 Position createNode(int value);
 Position insert(Position root, int value);
-int levelOrder(Position root);
+void inorderToFile(Position root, FILE* filePointer);
+void inorderToConsole(Position root);
 int replace(Position root);
-int random();
+int getRandomValue();
 
-// main
 int main() {
-	
-	Position root = NULL;
-	Position randRoot = NULL;
-	srand(time(0));
+    Position root = NULL;
+    Position rootRand = NULL;
+    srand(time(0));
 
-	root = insert(root, 2);
-	insert(root, 5);
-	insert(root, 7);
-	insert(root, 8);
-	insert(root, 11);
-	insert(root, 1);
-	insert(root, 4);
-	insert(root, 2);
-	insert(root, 3);
-	insert(root, 7);
+    root = insert(root, 2);
+    insert(root, 5);
+    insert(root, 7);
+    insert(root, 8);
+    insert(root, 11);
+    insert(root, 1);
+    insert(root, 4);
+    insert(root, 2);
+    insert(root, 3);
+    insert(root, 7);
 
-	levelOrder(root);
+    inorderToConsole(root);
+    printf("\n");
 
-	replace(root);
-	printf("\n");
-	levelOrder(root);
+    replace(root);
+    inorderToConsole(root);
+    printf("\n");
 
-	randRoot = insert(randRoot, random());
-	for (int i = 0; i < 10; i++) {
-		insert(randRoot, random());
-	}
+    rootRand = insert(rootRand, getRandomValue());
+    for (int i = 0; i < 10; i++) {
+        insert(rootRand, getRandomValue());
+    }
+    FILE* filePointer = fopen("inorder.txt", "w");
+    inorderToFile(rootRand, filePointer);
+    replace(rootRand);
 
-	printf("\n");
-	levelOrder(randRoot);
-
-	return 0;
+    fclose(filePointer);
+    return 0;
 }
 
 Position createNode(int value) {
-	Position newNode = NULL;
-	newNode = (Position)malloc(sizeof(Node));
-	if (!newNode) {
-		printf("Can't allocate memory!\n");
-		return NULL;
-	}
+    Position newNode = (Position)malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Can't allocate memory!\n");
+        return NULL;
+    }
 
-	newNode->element = value;
-	newNode->left = NULL;
-	newNode->right = NULL;
-	return newNode;
+    newNode->element = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
 }
 
 Position insert(Position root, int value) {
+    if (root == NULL) {
+        return createNode(value);
+    }
 
-	if (root == NULL) {
-		return createNode(value);
-	}
+    if (value >= root->element)
+        root->left = insert(root->left, value);
+    else if (value < root->element)
+        root->right = insert(root->right, value);
 
-	if (value < root->element) root->left = insert(root->left, value);
-	else if (value >= root->element) root->right = insert(root->right, value);
-
-	return root;
+    return root;
 }
 
-int levelOrder(Position root) {
-	if (root == NULL)
-		return;
+void inorderToFile(Position root, FILE* filePointer) {
+    if (root != NULL) {
+        inorderToFile(root->left, filePointer);
+        fprintf(filePointer, "%d ", root->element);
+        inorderToFile(root->right, filePointer);
+    }
+}
 
-	Position queue[100] = { 0 };
-	int front = 0, rear = 0;
-
-	queue[rear++] = root;
-
-	while (front < rear) {
-		Position current = queue[front++];
-
-		printf("%d ", current->element);
-
-		if (current->left != NULL)
-			queue[rear++] = current->left;
-
-		if (current->right != NULL)
-			queue[rear++] = current->right;
-	}
-	return EXIT_SUCCESS;
+void inorderToConsole(Position root) {
+    if (root != NULL) {
+        inorderToConsole(root->left);
+        printf("%d ", root->element);
+        inorderToConsole(root->right);
+    }
 }
 
 int replace(Position root) {
-	if (root == NULL) {
-		return 0;
-	}
-	int	leftElement = replace(root->left);
-	int rightElement = replace(root->right);
-	int originalElement = root->element;
+    int leftElement = 0;
+    int rightElement = 0;
+    int originalElement = 0;
 
-	root->element = leftElement + rightElement;
-	return originalElement + root->element;
+    if (root == NULL) {
+        return 0;
+    }
+
+    leftElement = replace(root->left);
+    rightElement = replace(root->right);
+    originalElement = root->element;
+
+    root->element = leftElement + rightElement;
+    return originalElement + root->element;
 }
 
-int random() {
-	int value = 0;
-	value = rand() % 81 + 10;
-	return value;
+int getRandomValue() {
+    return rand() % 81 + 10;
 }
